8350da3bee0ec5925a8ad27f509f432f11908252
koolon <89884320+koolon@users.noreply.github.com>
2021-09-03 13:06:54 +0900

    Update script.js

--- real-escape-game-v5/assets/js/script.js
+++ real-escape-game-v5/assets/js/script.js
@@ -1,4 +1,3 @@
-history.replaceState(null, document.getElementsByTagName('title')[0].innerHTML, null);
 if(history && history.pushState && history.state != undefined){
   history.pushState(null, null, null);
   window.addEventListener("popstate", function() {
