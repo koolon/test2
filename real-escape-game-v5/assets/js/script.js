7736f4ae8a91d6856c29a5044155057308c1bc62
koolon <89884320+koolon@users.noreply.github.com>
2021-09-03 12:48:16 +0900

    Update script.js

--- real-escape-game-v5/assets/js/script.js
+++ real-escape-game-v5/assets/js/script.js
@@ -1,5 +1,5 @@
 history.pushState(null, null, null);
-$(window).on("popstate", function (event) {
+$(window).addEventListener('popstate', function(event) {
   if (!event.originalEvent.state) {
     history.pushState(null, null, null);
     alert("戻れると思った？。");
