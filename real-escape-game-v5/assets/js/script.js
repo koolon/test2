5ef5104a919e3d10a075cce479730f1bb5d5d43b
koolon <89884320+koolon@users.noreply.github.com>
2021-09-03 13:14:04 +0900

    Update script.js

--- real-escape-game-v5/assets/js/script.js
+++ real-escape-game-v5/assets/js/script.js
@@ -1,11 +1,4 @@
-if(history && history.pushState && history.state != undefined){
-  history.pushState(null, null, null);
-  window.addEventListener("popstate", function() {
-    history.pushState(null, null, null);
-    window.onbeforeunload = function(e) {
-      return 'このページから離れますか？';
-    };
-    alert("戻れると思った？");
-  });
-}
-
+history.replaceState(null, null, null);
+window.addEventListener('popstate', function(e) {
+  alert('テスト');
+});
